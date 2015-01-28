be3478d42a0ae67df5eb9899f4d859ed9d9bf97c
luis-sarro <lsb@dia.uned.es>
2015-01-28 07:40:08 +0100

    lsb

--- line-identification/irtf/plot-reg.r
+++ line-identification/irtf/plot-reg.r
@@ -17,11 +17,12 @@ teff <- NULL
 logg <- NULL
 met <- NULL
 
-target <- "BT_SETTL_pb_clean.RData"
+target <- "BT-Settl-2011-IRTF.RData"
 load(target)
+
 for (i in 1:length(bp_clean))
   {
-    if (bp_clean[[i]]$stellarp[2] == -5 & bp_clean[[i]]$stellarp[3] == 0 )
+    if (bp_clean[[i]]$stellarp[2] == 5 & bp_clean[[i]]$stellarp[3] == 0 )
       {
         data <- rbind(data,bp_clean[[i]]$data[[1]][,2])
         teff <- rbind(teff,as.numeric(bp_clean[[i]]$stellarp[1]))
@@ -40,7 +41,7 @@ if (!convolved)
     data <- NULL
     for (i in 1:length(bp_clean))
       {
-        if (bp_clean[[i]]$stellarp[2] == -5 & bp_clean[[i]]$stellarp[3] == 0 )
+        if (bp_clean[[i]]$stellarp[2] == 5 & bp_clean[[i]]$stellarp[3] == 0 )
           {
             data <- rbind(data,bts_clean[[i]]$data[,2])
           }
@@ -51,17 +52,18 @@ if (!convolved)
 c <- SetupPalette(teff)
 
 
+top <- 0.00065; right<- 10000; left <- min(wav)
 filename <- "teff-snrinfty.r"
 source(filename)
-               
+
 for (j in 1:5){
   jpeg(paste("Feature",j,".jpg",sep=""),quality=100,width=1000, height=1000)
   par(mar=c(7,7,5,10))
-  plot(wav,data[1,], ty="l",col=c[1],ylim=c(0,0.035),main=paste("Feature ",j,sep=""),
+  plot(wav,data[1,], ty="l",col=c[1],xlim=c(left,right), ylim=c(0,top),main=paste("Feature ",j,sep=""),
        cex.axis=2, cex.lab=2, xlab="Lambda(Angstroms)", ylab="Flux(relative units)")
   for ( i in 2:(dim(data)[1]))
     {
-      lines(wav,data[i,]+(i-1)*0.0015,col=c[i])
+      lines(wav,data[i,]+(i-1)*0.000015,col=c[i])
     }
   rect(lims[j,1],lims[j,2],lims[j,3],lims[j,4],)
   rect(lims2[j,1],lims2[j,2],lims2[j,3],lims2[j,4],col="grey",density=5)
@@ -82,13 +84,13 @@ for (j in 1:5){
 
   jpeg(paste("All-Teff.jpg",sep=""),quality=100,width=1000, height=1000)
   par(mar=c(7,7,5,10))
-  plot(wav,data[1,], ty="l",col=c[1],ylim=c(0,0.035),main=paste("Feature ",j,sep=""),
+  plot(wav,data[1,], ty="l",col=c[1],xlim=c(left,right),ylim=c(0,top),main=paste("Feature ",j,sep=""),
        cex.axis=2, cex.lab=2, xlab="Lambda(Angstroms)", ylab="Flux(relative units)")
 
 for (j in 1:5){
   for ( i in 2:(dim(data)[1]))
     {
-      lines(wav,data[i,]+(i-1)*0.0015,col=c[i])
+      lines(wav,data[i,]+(i-1)*0.000015,col=c[i])
     }
   rect(lims[j,1],lims[j,2],lims[j,3],lims[j,4],)
   rect(lims2[j,1],lims2[j,2],lims2[j,3],lims2[j,4],col="grey",density=5)
