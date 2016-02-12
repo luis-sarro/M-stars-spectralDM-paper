355a112079c6a7e62dc0a7b2e0e69b9eac122445
luis-sarro <lsb@dia.uned.es>
2016-02-12 12:02:13 +0100

    jk

--- paper/figs/irtf-figs/irtf-figs.r
+++ paper/figs/irtf-figs/irtf-figs.r
@@ -6,22 +6,71 @@ tab3 <- read.table("Table3.txt",sep="|",colClasses = c("character",rep("numeric"
 lc <- rep(NA,dim(tab3)[1])
 tmp <- tab3[,1]
 mask <- grep("IV",tmp)
-lc[mask] <- 4
+lc[mask] <- 16
 tmp <- gsub("IV","",tmp)
 mask <- grep("V",tmp)
-lc[mask] <- 5
+lc[mask] <- 15
 tmp <- gsub("V","",tmp)
 mask <- grep("III",tmp)
-lc[mask] <- 3
+lc[mask] <- 17
 tmp <- gsub("III","",tmp)
 mask <- grep("II",tmp)
-lc[mask] <- 2
+lc[mask] <- 18
 tmp <- gsub("II","",tmp)
 mask <- grep("I",tmp)
-lc[mask] <- 1
+lc[mask] <- 18
 tmp <- gsub("I","",tmp)
 mask <- is.na(lc)
-lc[mask] <- 6
+lc[mask] <- 4
+irtf.lcs <- read.table("../../../names/IRTF-origweb.txt",sep=",")
+irtf.lcs[,1] <- gsub(" ","",irtf.lcs[,1])
+irtf.lcs[,1] <- gsub("[\t]","",irtf.lcs[,1])
+match <- match(df_M[,1],irtf.lcs[,1])
+pec <- is.na(match)
+tck <- which(pec)
+k <- sum(pec)
+for (i in 1:k)
+{
+  idx <- grep(substr(df_M[tck[i],1],1,10),irtf.lcs[,1],fixed=T)
+  print(paste(i," ",df_M[tck[i],1]," ", idx, " ",irtf.lcs[idx,1]))
+  print(match)
+  match[df_M[,1]==df_M[tck[i],1]] <- idx  
+  print(match)
+}
+irtf.lcs.sorted <- irtf.lcs[match,2]
+irtf.lcs.sorted <- gsub("[\t]","",irtf.lcs.sorted)
+irtf.lcs.sorted <- gsub(" ","",irtf.lcs.sorted)
+lc2 <- rep(NA,length(irtf.lcs.sorted))
+tmp <- irtf.lcs.sorted
+mask <- grep("IV",tmp)
+lc2[mask] <- 16
+tmp <- gsub("IV","",tmp)
+mask <- grep("V",tmp)
+lc2[mask] <- 15
+tmp <- gsub("V","",tmp)
+mask <- grep("III",tmp)
+lc2[mask] <- 17
+tmp <- gsub("III","",tmp)
+mask <- grep("II",tmp)
+lc2[mask] <- 18
+tmp <- gsub("II","",tmp)
+mask <- grep("I",tmp)
+lc2[mask] <- 18
+tmp <- gsub("I","",tmp)
+mask <- is.na(lc2)
+lc2[mask] <- 4
+
+
+
+cex.size <- .6
+pdf("../ordieres-fig1.pdf",width=8,height=8)
+plot(df_T_inf$T_teo,df_T_inf$KNN,pch=lc2,col="black",xlim=c(1500,4500),ylim=c(1500,4500),
+     xlab=expression(T[eff-lit]),ylab=expression(T[eff-est]),cex=cex.size)
+#points(df_T_inf$T_teo,df_T_10$MARS,pch=16,col="red",cex=cex.size)
+points(df_T_inf$T_teo,df_T_inf$`Rule-Regression`,pch=lc2,col="blue",cex=cex.size)
+#points(df_T_inf$T_teo,df_T_10$Chi2_10,pch=16,col="orange",cex=cex.size)
+abline(0,1)
+dev.off()
 
 pdf("../ordieres-fig4.pdf",width=8,height=5)
 par(cex.axis=1.0)
@@ -37,7 +86,7 @@ nf = layout(layoutmat,respect=T,
 )
 
 par(mar = c(0,5,1,0))
-plot(log10(df_T_inf$KNN),df_G$`Rule-Regression_50`,pch=16,cex=0.5,col="red",axes=F,xlab="",
+plot(log10(df_T_inf$KNN),df_G$`Rule-Regression_50`,pch=lc2,cex=0.5,col="red",axes=F,xlab="",
      ylab="log(g)",cex.lab=1.5, xlim=c(3.9,3.25), ylim=c(6,-1))
 points(log10(tab3[,2]),tab3[,3],cex=.5,pch=lc)
 box()
@@ -45,7 +94,7 @@ text(3.4,0,"GA-RR-50",pos=4,cex=0.7)
 axis(2)
 
 par(mar = c(0,0,1,4))
-plot(log10(df_T_inf$KNN),df_G$PLS_50,pch=16,cex=0.5,col="red",axes=F,xlab="",
+plot(log10(df_T_inf$KNN),df_G$PLS_50,pch=lc2,cex=0.5,col="red",axes=F,xlab="",
      ylab="log(g)",cex.lab=1.5, xlim=c(3.9,3.25), ylim=c(6,-1))
 points(log10(tab3[,2]),tab3[,3],cex=.5,pch=lc)
 box()
@@ -53,7 +102,7 @@ axis(4)
 text(3.4,0,"GA-PLS-50",pos=4,cex=0.7)
 
 par(mar = c(4,5,0,0))
-plot(log10(df_T_inf$KNN),df_G$NNR_50,pch=16,cex=0.5,col="blue",axes=F,xlab=expression(log(T[eff])),
+plot(log10(df_T_inf$KNN),df_G$NNR_50,pch=lc2,cex=0.5,col="blue",axes=F,xlab=expression(log(T[eff])),
      ylab="log(g)",cex.lab=1.5, xlim=c(3.9,3.25), ylim=c(6,-1))
 points(log10(tab3[,2]),tab3[,3],cex=.5,pch=lc)
 box()
@@ -63,7 +112,7 @@ text(3.4,0,"GA-NNR-50",pos=4,cex=0.7)
 box()
 #
 par(mar = c(4,0,0,4))
-plot(log10(df_T_inf$KNN),df_G$ICA_10,pch=16,cex=0.5,col="blue",axes=F,xlab=expression(log(T[eff])),
+plot(log10(df_T_inf$KNN),df_G$ICA_10,pch=lc2,cex=0.5,col="blue",axes=F,xlab=expression(log(T[eff])),
      ylab="log(g)",cex.lab=1.5, xlim=c(3.9,3.25), ylim=c(6,-1))
 points(log10(tab3[,2]),tab3[,3],cex=.5,pch=lc)
 box()
@@ -143,17 +192,40 @@ dev.off()
 
 pdf("M-ICA10.pdf")
 par(mar = c(6,6,1,1))
-plot(df_M$M_teo, df_M[,30],xlim=c(-.8,.8),ylim=c(-.8,.8),xlab="Literature Fe/H or M/H",ylab="M/H",cex.axis=1.5,cex.lab=1.5)
-  points(n3.m, df_M[,30],pch=16,col="orange") # Fe/H
-  points(nt8.m[,1], df_M[,30],pch=15,col="green") # Fe/H 1
-  points(nt8.m[,2], df_M[,30],pch=15,col="darkgreen") # Fe/H 2
-  points(ra.m[,1], df_M[,30],pch=17,col="cyan") # M/H
-  points(ra.m[,3], df_M[,30],pch=17,col="blue") # Fe/H
-  points(mann.m, df_M[,30],pch=18,col="red") # Fe/H
-  points(new.m, df_M[,30],pch=19,col="yellow") # Fe/H
-  points(gaidos.m, df_M[,30],pch=20,col="black") # Fe/H
+plot(df_M$M_teo, df_M[,30],pch=lc2,xlim=c(-.8,.8),ylim=c(-.8,.8),xlab="Literature Fe/H or M/H",ylab="M/H",cex.axis=1.5,cex.lab=1.5)
+  points(n3.m, df_M[,30],pch=lc2,col="orange") # Fe/H
+  points(nt8.m[,1], df_M[,30],pch=lc2,col="green") # Fe/H 1
+  points(nt8.m[,2], df_M[,30],pch=lc2,col="darkgreen") # Fe/H 2
+  points(ra.m[,1], df_M[,30],pch=lc2,col="cyan") # M/H
+  points(ra.m[,3], df_M[,30],pch=lc2,col="blue") # Fe/H
+  points(mann.m, df_M[,30],pch=lc2,col="red") # Fe/H
+  points(new.m, df_M[,30],pch=lc2,col="yellow") # Fe/H
+  points(gaidos.m, df_M[,30],pch=lc2,col="black") # Fe/H
 #  text(0,0.2,paste(colnames(df_M)[30],sum(abs(df_M[avail,i]) < 0.4,na.rm=T)))
   abline(0,1)
 dev.off()
+
+#####################################################
+pal <- colorRampPalette(c("yellow","red"),space = "rgb")
+SetupPalette<-function(c)
+{
+  nl <- 50
+  palette<-pal(nl)
+  #palette <- tim.colors(nl)
+  col <- c-min(c,na.rm=TRUE)
+  col <- col/max(col,na.rm=TRUE)
+  colour <- palette[as.integer(((nl-1)*col)+1)]
+  return(colour)
+}
+
+pdf("../ordieres-fig8.pdf",width=8,height=5)
+par(cex.axis=1.0)
+col <- SetupPalette(df_M$ICA_10)
+plot(log10(df_T_inf$KNN),df_G$`Rule-Regression_50`,pch=lc2,cex=0.5,col=col,xlab=expression(log(T[eff])),
+     ylab="log(g)",cex.lab=1.5, xlim=c(3.9,3.25), ylim=c(6,-1))
+points(log10(tab3[,2]),tab3[,3],cex=.5,pch=lc)
+text(3.4,0,"GA-RR-50",pos=4,cex=0.7)
+dev.off()
+
 
 
